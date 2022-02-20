72a01ca5a52b7e8ca913acf2ec6681ba30029ff1
Rashad Nelson <rashadnelson@gmail.com>
2022-02-20 12:58:06 -0500

    First commit - 02/20/22

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`)
+})
+
+
